8c22cb591fb1d8acdd670314922be44e0c852b9c
tigerhackmalayalam <thmalayalamchannel@gmail.com>
2020-09-05 15:36:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+•••••WELCOME TO "TIGER HACK MALAYALAM"•••••
+
+Link:- https://www.youtube.com/c/thmalayalam
+
+
+Make UNLIMITED Test Messeges In One Click.
+
+
+commands...
+•••••••••••
+
+$ apt update
+
+$ apt upgrade 
+
+$ pkg install git -y
+
+$ git clone 
+
+$ cd 
+
+$ bash 
+
+Then type any text   and click enter
+then Type the number how many times you want to repeat the text
+
+
+
+then enter 
+
+
+please wait for 5 seconds
+
+After finishing Type 
+
+$ cp text.txt /sdcard
+
+Then open your File Manager and Internal 
+There you can see a text.txt  file open that file 
+and copy the text and send to everything 
+
+
+
+•••••FOR EDUCATION PURPOSE ONLY•••••
---
+++ txt-msg.sh
@@ -0,0 +1,114 @@
+﻿#!/bin/bash
+clear
+rm -rf text.txt
+clear
+echo " "
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo " "
+echo "▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄
+ ▀▀▀██▀▀▀   ▀▀██▀▀    ██▀▀▀▀█  ██▀▀▀▀▀▀  ██▀▀▀▀██
+    ██        ██     ██        ██        ██    ██
+    ██        ██     ██  ▄▄▄▄  ███████   ███████
+    ██        ██     ██  ▀▀██  ██        ██  ▀██▄
+    ██      ▄▄██▄▄    ██▄▄▄██  ██▄▄▄▄▄▄  ██    ██
+    ▀▀      ▀▀▀▀▀▀      ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀▀ "
+echo "▄▄    ▄▄     ▄▄        ▄▄▄▄   ▄▄   ▄▄▄
+ ██    ██    ████     ██▀▀▀▀█  ██  ██▀
+ ██    ██    ████    ██▀       ██▄██
+ ████████   ██  ██   ██        █████
+ ██    ██   ██████   ██▄       ██  ██▄
+ ██    ██  ▄██  ██▄   ██▄▄▄▄█  ██   ██▄
+ ▀▀    ▀▀  ▀▀    ▀▀     ▀▀▀▀   ▀▀    ▀▀ "
+echo " " 
+echo "===================================================="
+echo " "
+echo ".....Script by Tiger Hack Malayalam....."
+echo " "
+echo "---------------------------------------------"
+echo "By Clicking ENTER you Agree That.."
+echo "This tool is using only for "
+echo "FOR EDUCATIONAL PURPOSE ONLY"
+echo "---------------------------------------------"
+echo " "
+echo " Enter the text messege you want to print :"
+
+echo ">>>"
+read text
+clear
+echo " "
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo " "
+echo "▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄
+ ▀▀▀██▀▀▀   ▀▀██▀▀    ██▀▀▀▀█  ██▀▀▀▀▀▀  ██▀▀▀▀██
+    ██        ██     ██        ██        ██    ██
+    ██        ██     ██  ▄▄▄▄  ███████   ███████
+    ██        ██     ██  ▀▀██  ██        ██  ▀██▄
+    ██      ▄▄██▄▄    ██▄▄▄██  ██▄▄▄▄▄▄  ██    ██
+    ▀▀      ▀▀▀▀▀▀      ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀▀ "
+echo "▄▄    ▄▄     ▄▄        ▄▄▄▄   ▄▄   ▄▄▄
+ ██    ██    ████     ██▀▀▀▀█  ██  ██▀
+ ██    ██    ████    ██▀       ██▄██
+ ████████   ██  ██   ██        █████
+ ██    ██   ██████   ██▄       ██  ██▄
+ ██    ██  ▄██  ██▄   ██▄▄▄▄█  ██   ██▄
+ ▀▀    ▀▀  ▀▀    ▀▀     ▀▀▀▀   ▀▀    ▀▀ "
+echo " "
+echo "===================================================="
+echo " "
+echo "How Many Times You Want To Print :" $text 
+echo ">>>"
+read count
+
+echo "**********************************************"
+echo "    Please wait...."
+echo "    Making "$count $text 
+echo "    Messages....."
+echo " "
+sleep 3
+for (( i=2; i<=$count; i++))
+do
+
+echo $text >>text.txt
+done
+clear
+echo " "
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo " "
+echo "▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄
+ ▀▀▀██▀▀▀   ▀▀██▀▀    ██▀▀▀▀█  ██▀▀▀▀▀▀  ██▀▀▀▀██
+    ██        ██     ██        ██        ██    ██
+    ██        ██     ██  ▄▄▄▄  ███████   ███████
+    ██        ██     ██  ▀▀██  ██        ██  ▀██▄
+    ██      ▄▄██▄▄    ██▄▄▄██  ██▄▄▄▄▄▄  ██    ██
+    ▀▀      ▀▀▀▀▀▀      ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀▀ "
+echo "▄▄    ▄▄     ▄▄        ▄▄▄▄   ▄▄   ▄▄▄
+ ██    ██    ████     ██▀▀▀▀█  ██  ██▀
+ ██    ██    ████    ██▀       ██▄██
+ ████████   ██  ██   ██        █████
+ ██    ██   ██████   ██▄       ██  ██▄
+ ██    ██  ▄██  ██▄   ██▄▄▄▄█  ██   ██▄
+ ▀▀    ▀▀  ▀▀    ▀▀     ▀▀▀▀   ▀▀    ▀▀ "
+echo " " 
+echo "===================================================="
+echo " "
+echo ".....Script by Tiger Hack Malayalam....."
+echo " "
+echo "-------------------------------------------------"
+echo "   Your "$count $text 
+echo "   Text File Created SUCCESSFULLY"
+echo "-------------------------------------------------"
+echo " "
+echo "   Now Type:- cp text.txt /sdcard "
+echo "   Now open your Internal storage  "
+echo "   There you can see "text.txt" File"
+echo "   Open that File And Copy the Numbers "
+echo "   And SEND TO ANYONE........."
+echo " "
+echo "================================================="
+echo " "
+echo "   For More Details.."
+echo "   Subscribe My Channel :- TIGER HACK MALAYALAM"
+echo "   Link:- https://www.youtube.com/c/thmalayalam "
+echo "   Email:- thmalayalamchannel@gmail.com "
+echo " "
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
